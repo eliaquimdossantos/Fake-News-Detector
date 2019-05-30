617a726f7ef117b9368d45831b6a7d9f704eb7b6
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-05-30 11:01:08 -0300

    Verção 1.0 concluida

--- src/br/ufrn/imd/lpii/application/SampleController.java
+++
@@ -1,5 +0,0 @@
-package br.ufrn.imd.lpii.application;
-
-public class SampleController {
-	
-}
--- src/br/ufrn/imd/lpii/domain/NewsInterface.java
+++
@@ -1,8 +0,0 @@
-package br.ufrn.imd.lpii.domain;
-
-public interface NewsInterface {
-	public void addParagraph(String paragraph);
-	public String acquireNews();
-	public String acquireParagraph(int number);
-	public int acquireTotalParagraphs();
-}
